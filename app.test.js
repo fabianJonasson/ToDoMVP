1d080a74d8f5879761865d0a071b12d9ae95166f
fabianJonasson <fabian.jonasson@kuberacad.se>
2023-10-05 13:16:18 +0200

    added unit tests for POST / and DELETE /id

--- app.test.js
+++ app.test.js
@@ -22,6 +22,35 @@ test('GET /id', done => {
     .end(done); 
 });
 
+test('POST /', async(done) => {
+    const task = {
+        id: tasks.length+1,
+        desc: 'new test', 
+        date: '2023-10-05'
+    };
+
+    const count = await tasks.length();
+    await request(app)
+        .post('/')
+        .send(task)
+
+    const newCount = await tasks.length()
+    expect(newCount).toBe(count + 1);
+    done();
+});
+
+test('DELETE /id', done => {
+    const tasks = [
+        {id: 1, desc: 'task 1', date: '2023-10-05'},
+        {id: 2, desc: 'task 2', date: '2023-10-05'}
+    ];
+
+    request(app)
+    .delete('/2')
+    .expect(200, tasks[1] == undefined)
+    .end(done); 
+});
+
 test('GET /jibberish', done => {
     request(app)
     .get('/asdijopswko')
