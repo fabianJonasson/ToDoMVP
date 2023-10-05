4a978349a32b51ef3016f75cdd9f28f17050a6ab
fabianJonasson <fabian.jonasson@kuberacad.se>
2023-10-05 12:50:24 +0200

    added test for route GET /id

--- app.test.js
+++ app.test.js
@@ -1,12 +1,25 @@
 const request = require('supertest');
 
 const app = require('./app');
+const tasks = require('./index').tasks;
 
 test('Im alive!', done => {
     request(app)
     .get('/')
     .expect(200)
     .end(done);
+});
+
+test('GET /id', done => {
+    const tasks = [
+        {id: 1, desc: 'task 1', date: '2023-10-05'},
+        {id: 2, desc: 'task 2', date: '2023-10-05'}
+    ];
+    
+    request(app)
+    .get('/2')
+    .expect(200, tasks[1].desc === 'task 2')
+    .end(done); 
 });
 
 test('GET /jibberish', done => {
